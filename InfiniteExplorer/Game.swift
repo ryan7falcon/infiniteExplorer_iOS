f25bc9a7d3896e4ca714100ba7a55cf9e4971e56
Ryan Falcon <ryan7falcon@gmail.com>
2018-01-02 14:42:15 -0500

    added comments

--- InfiniteExplorer/Game.swift
+++ InfiniteExplorer/Game.swift
@@ -5,10 +5,12 @@
 //  Created by Ryan Galimova on 2017-12-27.
 //  Copyright © 2017 Ryan Galimova. All rights reserved.
 //
+// The game class that will hold the current world, username and score + other info needed by the game
 
 import UIKit
 
 class Game: NSObject {
+    // the current world that the user is located in
     var world: World!
     
     override init() {
